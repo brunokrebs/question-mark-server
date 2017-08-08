9be8b24d77180939ead354323bed44cf458b662a
Bruno Krebs <bruno.krebs@auth0.com>
2017-08-08 15:21:21 -0300

    First commit with base structure

---
+++ src/main/java/com/auth0/questionmark/QuestionmarkApplication.java
@@ -0,0 +1,12 @@
+package com.auth0.questionmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionmarkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/auth0/questionmark/QuestionmarkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.auth0.questionmark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuestionmarkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
